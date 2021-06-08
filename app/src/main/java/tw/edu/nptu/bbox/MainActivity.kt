c57fb25e23a92c9fd26d472bc291dd684d0ec243
TingYu Yan <46291423+tingyus995@users.noreply.github.com>
2021-06-09 00:24:25 +0800

    added hamburger menu on BottleListFragment.

--- app/src/main/java/tw/edu/nptu/bbox/MainActivity.kt
+++ app/src/main/java/tw/edu/nptu/bbox/MainActivity.kt
@@ -2,27 +2,32 @@ package tw.edu.nptu.bbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import tw.edu.nptu.bbox.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val drawerLayout = binding.drawerLayout
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.bottleList))
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-
-
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
